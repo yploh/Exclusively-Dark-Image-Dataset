68db028d0485c8d86268284d380b5ccb8530ae36
yploh <39718759+yploh@users.noreply.github.com>
2019-06-01 20:19:52 +0800

    Add files via upload

--- llgp_enhance/demo.m
+++ llgp_enhance/demo.m
@@ -1,12 +1,12 @@
 % GP enhancement demo
 
-cnn_model = '.\cnnmodel.mat';
+cnn_model = '.\cnnmodel.mat';                                % pretrained CNN model
 image_name = '.\2015_00003.png';
 
 load(cnn_model);                                             % load CNN model
 image = imread(image_name);
 
-gp_res = gp_en(image,net);
+gp_res = gp_en(image,net);                                   % enhancement function
 
 figure
 subplot(1,2,1)
